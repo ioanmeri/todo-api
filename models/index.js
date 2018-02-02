abb4cc31e4658f5f80d0f4ff4f71e2337989dae2
Ioannis Mer <ioannismeri@gmail.com>
2018-02-02 01:47:50 +0000

    Update database connection to include local OR online DB for dev. and prod.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
+var url = process.env.DATABASEURL || "mongodb://localhost/todo-api"
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect(url);
+
 
 mongoose.Promise = Promise;
 
